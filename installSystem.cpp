f322fd4e40ac35c43c2f8c2d569a69bfb2ce80f1
duckyondiscord <ferenczigyozo@nagyimreiskola.ro>
2022-05-31 16:15:45 +0300

    Made a few changes to the install part

--- installSystem.cpp
+++ installSystem.cpp
@@ -2,14 +2,56 @@
 #include <stdlib.h>
 #include <string>
 
+int isSubstring(std::string s1, std::string s2)
+{
+    int M = s1.length();
+    int N = s2.length();
+ 
+    /* A loop to slide pat[] one by one */
+    for (int i = 0; i <= N - M; i++) {
+        int j;
+ 
+        /* For current index i, check for
+ pattern match */
+        for (j = 0; j < M; j++)
+            if (s2[i + j] != s1[j])
+                break;
+ 
+        if (j == M)
+            return i;
+    }
+ 
+    return -1;
+}
+ 
+
 void installSystem()
 {
     using namespace std;
+    string diskPath;
+    string command;
     cout << "Enter the disk you used previously(raw disk name, no partition numbers):";
+    cin >> diskPath;
+    if(isSubstring("nvme", diskPath) != -1 || isSubstring("mmcblk", diskPath) != -1)
+    {
+        command = "mount " + diskPath + "p3";
+        system(command.c_str());
+    }
+    else
+    {
+        command = "mount " + diskPath + "3";
+        system(command.c_str());
+    }
+
+    system("pacstrap /mnt base linux linux-firmware sudo nano");
+    system("genfstab -U /mnt >> /mnt/etc/fstab");
+    
+
+    
     
 }
 
 int main()
 {
-
+    installSystem();
 }
