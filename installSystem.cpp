b4ef3e23656fc899101dca627961647921c9d925
ducky <94759537+duckyondiscord@users.noreply.github.com>
2022-05-31 14:51:42 +0300

    Initial commit

---
+++ installSystem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+void installSystem()
+{
+    using namespace std;
+    cout << "Enter the disk you used previously(raw disk name, no partition numbers):";
+    
+}
+
+int main()
+{
+
+}
